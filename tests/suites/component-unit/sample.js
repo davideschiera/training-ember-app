fbcebb909beb2c0f7686bc3b12350e83fc34dbd3
Davide Schiera <davide@sysdig.com>
2016-05-24 15:54:46 -0700

    Component unit testing.

--- tests/suites/component-unit/sample.js
+++ tests/suites/component-unit/sample.js
@@ -11,12 +11,55 @@
         }
     });
 
-    TestBuddy.test('Sample', function() {
+    TestBuddy.test('Verify data', function() {
+        expect(1);
+
         // define template and model for it
-        componentUnit.setup();
+        componentUnit.setup(
+            // template
+            '{{dashboard-data-panel data=data}}',
+
+            // model
+            {
+                data: [ 'something', 'different' ]
+            },
+
+            // actions
+            {}
+        );
 
         andThen(function() {
-            ok(false);
+            strictEqual(find('span').text(), 'something,different', 'Expected data');
+        });
+    });
+
+    TestBuddy.onlyTest('Reload', function() {
+        expect(1);
+
+        var reloaded = false;
+
+        // define template and model for it
+        componentUnit.setup(
+            // template
+            '{{dashboard-data-panel data=data reload="testingReload"}}',
+
+            // model
+            {
+                data: [ 'something', 'different' ]
+            },
+
+            // actions
+            {
+                testingReload: function() {
+                    reloaded = true;
+                }
+            }
+        );
+
+        click('button');
+
+        andThen(function() {
+            strictEqual(reloaded, true, 'Expected reloaded');
         });
     });
 
