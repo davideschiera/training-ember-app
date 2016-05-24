c4115258ee1005431b6de67fdeaf38b21772a1e3
Davide Schiera <davide@sysdig.com>
2016-05-24 11:18:03 -0700

    Simplification of dahboard route.

--- app/scripts/routes/dashboards/dashboard.js
+++ app/scripts/routes/dashboards/dashboard.js
@@ -1,28 +1,8 @@
 SampleApp.DashboardsDashboardRoute = Ember.Route.extend({
-    ready: false,
-    transition: null,
-
-    beforeModel: function() {
-        if (this.transition === null) {
-            Ember.run.later(this, function() {
-                this.ready = true;
-                this.transition.retry();
-            }, 2000);
-        }
-    },
-
     model: function() {
         return {
-            name: 'My Dashboard',
-            ready: this.ready
+            name: 'My Dashboard'
         };
-    },
-
-    afterModel: function(model, transition) {
-        if (model.ready === false) {
-            this.transition = transition;
-            transition.abort();
-        }
     },
 
     setupController: function(controller, model) {
